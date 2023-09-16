98e398b026da6622988044d2ff0cf89d8fedee7d
alexeituszowski <atuszovskii@licard.com>
2023-09-16 11:28:08 +0300

    task 6

--- UIKitMarathon6/ViewController.swift
+++ UIKitMarathon6/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let rectView: UIView = {
+        let view = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 80, height: 80)))
+        view.backgroundColor = .tintColor
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    lazy var dynamicAnimator = UIDynamicAnimator(referenceView: view)
+    var snap: UISnapBehavior?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(rectView)
+        
+        rectView.center = view.center
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tap))
+        view.addGestureRecognizer(tapGesture)
     }
+    
+    @objc
+    func tap(_ tapGesture: UITapGestureRecognizer) {
+        let location = tapGesture.location(in: view)
+        if let snap {
+            dynamicAnimator.removeBehavior(snap)
+        }
 
-
+        let snap = UISnapBehavior(item: rectView, snapTo: location)
+        snap.damping = 1
+        dynamicAnimator.addBehavior(snap)
+        
+        self.snap = snap
+    }
 }
 
